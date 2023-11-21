1e24c3c63ba52860c38f21c1d5d74a83f46c928a
George <tsintsadze7@gmail.com>
2023-11-21 18:25:29 +0400

    feat: paste main code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css"
 
 function App() {
-  return <div className="App">Hello</div>
+  return (
+    <div id="app-container" className="app-container">
+      <input type="text" id="search-input" className="search-input" size={40} />
+
+      <div className="posts_container" id="posts_container">
+        {/* JS inserts posts here */}
+        {/* On post click open post modal */}
+      </div>
+      <div className="post-navigation-container" id="post-navigation-container">
+        <button className="arrow" id="left-arrow" disabled>
+          {"<<<"}
+        </button>
+        <span className="page-number" id="page-number">
+          1
+        </span>
+        <button className="arrow" id="right-arrow">
+          {">>>"}
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
