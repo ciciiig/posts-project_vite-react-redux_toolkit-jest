b911886a4d38d65e6e8d1448d24324524f247c0b
George <tsintsadze7@gmail.com>
2023-11-25 03:55:33 +0400

    feat: add missing types

--- src/utils/filterPosts.ts
+++ src/utils/filterPosts.ts
@@ -1,6 +1,9 @@
-import { Posts } from "../features/posts/postsSlice"
+import { Posts, PostsState } from "../features/posts/postsSlice"
 
-export const filterPosts = (posts: Posts[], searchValue: string) =>
+export const filterPosts = (
+  posts: Posts[],
+  searchValue: PostsState["searchValue"],
+) =>
   posts.filter(
     (post) =>
       post.title.includes(searchValue) || post.body.includes(searchValue),
--- src/utils/getCurrentPostsState.ts
+++ src/utils/getCurrentPostsState.ts
@@ -1,7 +1,13 @@
 import { filterPosts } from "./filterPosts.js"
 import config from "../../config.json"
+import { Posts, PostsState } from "../features/posts/postsSlice.js"
+import { PaginationState } from "../features/pagination/paginationSlice.js"
 
-export const getCurrentPostsState = (allPosts, currentPage, searchValue) => {
+export const getCurrentPostsState = (
+  allPosts: Posts[],
+  currentPage: PaginationState["currentPage"],
+  searchValue: PostsState["searchValue"],
+) => {
   const posts = searchValue ? filterPosts(allPosts, searchValue) : allPosts
 
   const maxPages = Math.ceil(posts.length / config.maxPostsPerPage) // for test: 100 / 12 = 8.333 -> 9
