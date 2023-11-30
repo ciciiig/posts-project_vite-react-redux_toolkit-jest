d878ea49c47066ef9ba3ec4a57aa5f1472b9ef16
George <tsintsadze7@gmail.com>
2023-11-30 18:38:12 +0400

    fix: Input import

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react"
+import { Input } from "../Input/Input"
 import { PostsList } from "../PostsList/PostsList"
 import { Pagination } from "../Pagination/Pagination"
 
 export const Posts: FC = () => {
   return (
     <>
-      <input />
+      <Input />
       <PostsList />
       <Pagination />
     </>
