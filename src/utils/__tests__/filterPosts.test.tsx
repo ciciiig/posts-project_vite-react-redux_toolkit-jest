7e4d9850164f4aa9f5e9aeabbcbe7a3747826879
George <tsintsadze7@gmail.com>
2023-12-05 04:34:10 +0400

    feat

--- src/utils/__tests__/filterPosts.test.tsx
+++ src/utils/__tests__/filterPosts.test.tsx
@@ -85,13 +85,24 @@ describe("Test filterPosts function", () => {
     currentState = clone(initialState)
   })
 
-  it("filters posts by search value", () => {
+  it("filters posts by search value in body", () => {
     expect(filterPosts(currentState, "dolore placeat")).toEqual([
       {
         userId: 1,
         id: 7,
         title: "magnam facilis autem",
         body: "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
+      },
+    ])
+  })
+
+  it("filters posts by search value in title", () => {
+    expect(filterPosts(currentState, "qui eum")).toEqual([
+      {
+        userId: 1,
+        id: 8,
+        title: "dolorem dolore est ipsam",
+        body: "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae",
       },
     ])
   })
