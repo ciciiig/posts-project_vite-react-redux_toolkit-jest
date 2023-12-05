7cc114d0f6d4da6fa93331f5f9df9fe3529ddf91
George <tsintsadze7@gmail.com>
2023-12-05 07:19:19 +0400

    feat: testing fetch, not completed

---
+++ src/services/post/__tests__/post.test.ts
@@ -0,0 +1,30 @@
+import { postService } from "../post"
+
+describe("Test postService", () => {
+  const data = [
+    {
+      userId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+  ]
+
+  beforeEach(() => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(data),
+      }),
+    )
+  })
+
+  it("Get post fetches and returns posts", async () => {
+    const result = await postService.getPost()
+
+    expect(fetch).toHaveBeenCalledWith(
+      "https://jsonplaceholder.typicode.com/posts",
+    )
+    expect(result).toEqual(data)
+  })
+})
